e9b5bd2a538488a56269ac0bd91ddbf0ddba1d84
Gryffindor_Dodie <sultanovartur1873@gmail.com>
2022-06-02 10:05:16 +0300

    1

--- test.c
+++ test.c
@@ -28,43 +28,26 @@ int main(int argc, char *argv[]) {
         n = 0;
         l = 0;
         if(format[j] == 'n') {
-            char tmp[8];
-            int counter = 1;
-            for (int y = 0; y < str_count; y++, counter++, l++, n++) {
-                expr(counter, tmp);
-                strcat(out, tmp); // Реализовать построчную фигню (strcat)
-                l += 7;
-                for(; in[n] != '\n'; n++, l++) {
-                    out[l] = in[n];
-                }
-                out[l] = in[n];
-            }
+            
         }
     }
     printf("%s\n", out);
-    
 }
 
 
 char *expr(int counter, char *tmp) {
     if (counter < 10 && counter > 0) {
         sprintf(tmp, "     %d ", counter);
-        tmp[8] = '\0';    
     } else if (counter >= 10 && counter < 100) {
         sprintf(tmp, "    %d ", counter);
-        tmp[8] = '\0';    
     } else if (counter >= 100 && counter < 1000) {
-        sprintf(tmp, "   %d ", counter);
-        tmp[8] = '\0';    
+        sprintf(tmp, "   %d ", counter);  
     } else if (counter >= 1000 && counter < 10000) {
-        sprintf(tmp, "  %d ", counter);
-        tmp[8] = '\0';    
+        sprintf(tmp, "  %d ", counter);  
     } else if (counter >= 10000 && counter < 100000) {
-        sprintf(tmp, " %d ", counter);
-        tmp[8] = '\0';    
+        sprintf(tmp, " %d ", counter);  
     } else if (counter >= 100000 && counter < 1000000) {
         sprintf(tmp, "%d ", counter);
-        tmp[8] = '\0';
     } 
     // else {
     //     printf("Слишком много\n");
